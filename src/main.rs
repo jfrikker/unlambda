e247fd0d0fc2b62f7a51eb40169ad8ed4fc23cc9
Joe Frikker <jfrikker@gmail.com>
2022-04-28 23:59:10 -0400

    continuations

---
+++ src/main.rs
@@ -0,0 +1,175 @@
+use std::{io::{stdin, Read}, fs::File, env::args, rc::Rc};
+
+use parser::AstNode;
+
+use crate::parser::parse;
+
+mod parser;
+
+fn main() {
+    let mut code = String::new();
+    File::open(args().nth(1).unwrap()).unwrap().read_to_string(&mut code).unwrap();
+    let parsed = parse(&code).unwrap().1;
+
+    println!("{:?}", parsed);
+    println!("{:?}", Unlambda::default().run(parsed.into()));
+}
+
+#[derive(Debug, PartialEq)]
+enum Value {
+    Char(char),
+    Continuation(Rc<Continuation>),
+    CreateContinuation,
+    Distribute0,
+    Distribute1(Rc<Value>),
+    Distribute2(Rc<Value>, Rc<Value>),
+    Identity,
+    Lazy0,
+    Lazy1(MaybeEvaluated),
+    Read,
+    PrintCC,
+    Term,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+enum MaybeEvaluated {
+    Evaluated(Rc<Value>),
+    Unevaluated(Rc<AstNode>),
+}
+
+impl From<Rc<Value>> for MaybeEvaluated {
+    fn from(val: Rc<Value>) -> Self {
+        Self::Evaluated(val)
+    }
+}
+
+impl <'a> From<&'a Rc<Value>> for MaybeEvaluated {
+    fn from(val: &'a Rc<Value>) -> Self {
+        Self::Evaluated(val.clone())
+    }
+}
+
+impl From<Value> for MaybeEvaluated {
+    fn from(val: Value) -> Self {
+        Self::Evaluated(val.into())
+    }
+}
+
+impl From<Rc<AstNode>> for MaybeEvaluated {
+    fn from(val: Rc<AstNode>) -> Self {
+        Self::Unevaluated(val)
+    }
+}
+
+impl <'a> From<&'a Rc<AstNode>> for MaybeEvaluated {
+    fn from(val: &'a Rc<AstNode>) -> Self {
+        Self::Unevaluated(val.clone())
+    }
+}
+
+impl From<AstNode> for MaybeEvaluated {
+    fn from(val: AstNode) -> Self {
+        Self::Unevaluated(val.into())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Continuation {
+    Apply1(MaybeEvaluated, Rc<Continuation>),
+    Apply2(Rc<Value>, Rc<Continuation>),
+    Distribute(Rc<Value>, Rc<Value>, Rc<Continuation>),
+    Lazy0(MaybeEvaluated, Rc<Continuation>),
+    End,
+}
+
+#[derive(Default)]
+struct Unlambda {
+    current_char: Option<char>,
+}
+
+impl Unlambda {
+    fn run(&mut self, prog: Rc<AstNode>) -> MaybeEvaluated {
+        let (mut prog, mut continuation) = self.step(prog.into(), Continuation::End.into());
+        while *continuation != Continuation::End {
+            //println!("{:?} -- {:?}", prog, continuation);
+            (prog, continuation) = self.step(prog, continuation);
+        }
+        prog
+    }
+
+    fn step(&mut self, prog: MaybeEvaluated, continuation: Rc<Continuation>) -> (MaybeEvaluated, Rc<Continuation>) {
+        match prog {
+            MaybeEvaluated::Unevaluated(ast) => {
+                match ast.as_ref() {
+                    AstNode::Apply(f, a) => (f.into(),
+                        Rc::new(Continuation::Apply1(a.into(), continuation))),
+                    AstNode::Char(c) => (Value::Char(*c).into(), continuation),
+                    AstNode::Identity => (Value::Identity.into(), continuation),
+                    AstNode::Distribute => (Value::Distribute0.into(), continuation),
+                    AstNode::Lazy => (Value::Lazy0.into(), continuation),
+                    AstNode::Term => (Value::Term.into(), continuation),
+                    AstNode::Read => (Value::Read.into(), continuation),
+                    AstNode::PrintCC => (Value::PrintCC.into(), continuation),
+                    AstNode::Continuation => (Value::CreateContinuation.into(), continuation),
+                }
+            }
+            MaybeEvaluated::Evaluated(v) => self.continu(continuation, v.clone())
+        }
+    }
+
+    fn maybe_apply(&mut self, func: Rc<Value>, arg: MaybeEvaluated, continuation: Rc<Continuation>) -> (MaybeEvaluated, Rc<Continuation>) {
+        match &*func {
+            Value::Lazy0 => (Value::Lazy1(arg).into(), continuation),
+            Value::Lazy1(f) => (arg, Continuation::Lazy0(f.clone(), continuation).into()),
+            _ => (arg, Continuation::Apply2(func, continuation).into())
+        }
+    }
+
+    fn apply_value(&mut self, func: Rc<Value>, arg: Rc<Value>, continuation: Rc<Continuation>) -> (MaybeEvaluated, Rc<Continuation>) {
+        match func.as_ref() {
+            Value::Char(c) => {
+                print!("{}", c);
+                (arg.into(), continuation)
+            }
+            Value::Continuation(c) => (arg.into(), c.clone()),
+            Value::CreateContinuation => (Value::Continuation(continuation.clone()).into(), Continuation::Apply2(arg.into(), continuation).into()),
+            Value::Distribute0 => (Value::Distribute1(arg).into(), continuation),
+            Value::Distribute1(f1) => (Value::Distribute2(f1.clone(), arg).into(), continuation),
+            Value::Distribute2(f1, f2) => (arg.clone().into(),
+                Rc::new(Continuation::Apply2(f1.clone(), Continuation::Distribute(f2.clone(), arg, continuation).into()))),
+            Value::Identity => (arg.into(), continuation),
+            Value::Lazy0 => (Value::Lazy1(arg.into()).into(), continuation),
+            Value::Lazy1(f) => (f.clone(), Continuation::Apply1(arg.into(), continuation).into()),
+            Value::PrintCC => {
+                if let Some(c) = self.current_char {
+                    print!("{}", c);
+                    (arg.into(), continuation)
+                } else {
+                    (Value::Term.into(), continuation)
+                }
+            }
+            Value::Term => (func.into(), continuation),
+            Value::Read => {
+                let mut buf = [0];
+                if let Ok(_) = stdin().read(&mut buf) {
+                    self.current_char = Some(buf[0] as char);
+                    self.apply_value(arg, Value::Identity.into(), continuation)
+                } else {
+                    self.current_char = None;
+                    self.apply_value(arg, Value::Term.into(), continuation)
+                }
+            }
+        }
+    }
+    
+    fn continu(&mut self, continuation: Rc<Continuation>, value: Rc<Value>) -> (MaybeEvaluated, Rc<Continuation>) {
+        match continuation.as_ref() {
+            Continuation::End => panic!("Shouldn't have gotten here"),
+            Continuation::Apply1(arg, next) => self.maybe_apply(value, arg.clone(), next.clone()),
+            Continuation::Apply2(func, next) => self.apply_value(func.clone(), value, next.clone()),
+            Continuation::Distribute(f2, arg, next) => (MaybeEvaluated::Evaluated(arg.clone()),
+                Rc::new(Continuation::Apply2(f2.clone(), Rc::new(Continuation::Apply2(value, next.clone()))))),
+            Continuation::Lazy0(f, next) => (f.clone(), Rc::new(Continuation::Apply1(MaybeEvaluated::Evaluated(value), next.clone()))),
+        }
+    }
+}
---
+++ src/parser.rs
@@ -0,0 +1,49 @@
+use std::rc::Rc;
+
+use nom::{character::complete::{anychar, none_of}, IResult, multi::many1_count};
+
+#[derive(Debug, PartialEq)]
+pub enum AstNode {
+    Apply(Rc<AstNode>, Rc<AstNode>),
+    Char(char),
+    Continuation,
+    Distribute,
+    Identity,
+    Lazy,
+    PrintCC,
+    Read,
+    Term,
+}
+
+pub fn parse(i: &str) -> IResult<&str, Rc<AstNode>> {
+    let (i, c) = anychar(i)?;
+    match c {
+        '`' => apply(i),
+        'c' => Ok((i, Rc::new(AstNode::Continuation))),
+        'd' => Ok((i, Rc::new(AstNode::Lazy))),
+        'i' => Ok((i, Rc::new(AstNode::Identity))),
+        's' => Ok((i, Rc::new(AstNode::Distribute))),
+        'v' => Ok((i, Rc::new(AstNode::Term))),
+        '@' => Ok((i, Rc::new(AstNode::Read))),
+        '|' => Ok((i, Rc::new(AstNode::PrintCC))),
+        '.' => print_char(i),
+        '\n' => parse(i),
+        ' ' => parse(i),
+        '#' => {
+            let (i, _) = many1_count(none_of("\n"))(i)?;
+            parse(i)
+        }
+        x => panic!("invalid character {}", x),
+    }
+}
+
+fn apply(i: &str) -> IResult<&str, Rc<AstNode>> {
+    let (i, f) = parse(i)?;
+    let (i, a) = parse(i)?;
+    Ok((i, Rc::new(AstNode::Apply(f, a))))
+}
+
+fn print_char(i: &str) -> IResult<&str, Rc<AstNode>> {
+    let (i, c) = anychar(i)?;
+    Ok((i, Rc::new(AstNode::Char(c))))
+}
